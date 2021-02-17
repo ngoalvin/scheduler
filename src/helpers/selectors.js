e0c3b20f0507e87815e6441ceac0b2f4e0429e0e
Alvin Ngo <ngoalvin97@gmail.com>
2021-02-17 14:44:00 -0500

    add selectors.js and test for it

---
+++ src/helpers/selectors.js
@@ -0,0 +1,6 @@
+export function getAppointmentsForDay({ days, appointments }, day) {
+  const filteredDay = days.find((filterDay) => filterDay.name === day);
+  return filteredDay
+    ? filteredDay.appointments.map((id) => appointments[id])
+    : [];
+}
