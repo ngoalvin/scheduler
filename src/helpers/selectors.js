7117b3f9bcb8b4de3b77e7023d34264eb5194096
Alvin Ngo <ngoalvin97@gmail.com>
2021-02-18 16:18:18 -0500

    add getInterviewersForDay

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -4,3 +4,18 @@ export function getAppointmentsForDay({ days, appointments }, day) {
     ? filteredDay.appointments.map((id) => appointments[id])
     : [];
 }
+
+export function getInterview( {interviewers}, interview) {
+  if (!interview) {
+    return null;
+  }
+  const id = interview.interviewer; 
+  return {...interview, interviewer: interviewers[id]}
+}
+
+export function getInterviewersForDay ({days, interviewers }, day) {
+  const filteredDay = days.find((filterDay) => filterDay.name === day);
+  return filteredDay
+    ? filteredDay.interviewers.map((id) => interviewers[id])
+    : [];
+}
