31c9936d3aca155e33df32328270f0c6dd355af8
Alvin Ngo <ngoalvin97@gmail.com>
2021-02-18 16:18:00 -0500

    import useVisualMode and use short-circuit to display different modes

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,19 +3,42 @@ import "components/Appointment/styles.scss";
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
+import useVisualMode from "hooks/useVisualMode";
+import Form from "components/Appointment/Form";
+
 
 
 export default function Appointment (props) {
+  
+  const { interview, time, id, interviewers } = props;
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";  
 
-  const { interview, time, id } = props;
+  const { mode, transition, back } = useVisualMode(
+    interview ? SHOW : EMPTY
+  );
 
-  const isInterview = interview ? <Show student={interview.student} interviewer={interview.interviewer.name} /> : <Empty />;
+  const onAdd = () => {
+    transition("CREATE");
+  }
+  console.log(interviewers, "HERE")
+  // const isInterview = mode === SHOW ? 
+  // <Show student={interview.student} interviewer={interview.interviewer.name} /> : 
+  // <Empty onAdd={onAdd} />;
 
-  console.log(props.interview, "HERE");
   return (
     <article className="appointment">
       <Header time={time}/>
-      {isInterview}
+      {/* {isInterview} */}
+      {mode === EMPTY && <Empty onAdd={onAdd} />}
+      {mode === SHOW && (
+        <Show
+          student={interview.student}
+          interviewer={interview.interviewer.name}
+        />
+      )}
+      {mode === CREATE && <Form interviewers={[]} onCancel={back} />}
     </article>
   )
 }
