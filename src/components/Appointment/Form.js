ccb202ee8ddd151703a8095492bb67ca6081e3ef
Alvin Ngo <ngoalvin97@gmail.com>
2021-02-19 13:28:36 -0500

    add a checker in onSubmit to see if name/interviewer exists

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -21,7 +21,11 @@ export default function Form(props) {
 
   const onSubmit = (event) => {
     event.preventDefault();
-    onSave(name, interviewer);
+    if (name && interviewer) {
+      onSave(name, interviewer);
+    } else {
+      cancel();
+    }
   }
 
 
