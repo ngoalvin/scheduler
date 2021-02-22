ec34e8ded5c1b088cf37bc5084f53793614f9413
Alvin Ngo <ngoalvin97@gmail.com>
2021-02-22 12:42:47 -0500

    add propTypes checker

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,8 +1,13 @@
 import React from "react";
 import InterviewerListItem from "components/InterviewerListItem";
 import "components/InterviewerList.scss"
+import {PropTypes} from "prop-types";
 
 export default function InterviewerList(props){
+  
+  InterviewerList.propTypes = {
+    interviewers: PropTypes.array.isRequired
+  };
 
   const { interviewers } = props;
   const interviewerRenderer = interviewers.map(interviewer => {
