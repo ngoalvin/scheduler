ed7bb65a714b0008216994bb32e75e22c1438f8f
Alvin Ngo <ngoalvin97@gmail.com>
2021-02-23 16:16:27 -0500

    add data-testid

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -72,7 +72,7 @@ export default function Appointment (props) {
   }, [interview, transition, mode])
 
   return (
-    <article className="appointment">
+    <article className="appointment" data-testid="appointment">
       <Header time={time}/>
 
       {
