5e8d943ca4b13bef797bb01b0b7b2d19952a931c
Alvin Ngo <ngoalvin97@gmail.com>
2021-02-23 16:19:08 -0500

    remove reducer and import reducer from seperate file

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,49 +1,10 @@
 import { useEffect, useReducer } from "react";
 import axios from "axios";
-
-const updateDays = function (id, appointments, state) {
-  const days = state.days.map((day) => {
-    if (day.appointments.includes(id)) {
-      return {...day, spots: day.appointments.filter((appointmentId) => {
-       return (appointments[appointmentId].interview === null)
-      }).length }
-    } else {
-      return day;
-    }
-  })
-  return days
-}
-
-const SET_DAY = "SET_DAY";
-const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
-const SET_INTERVIEW = "SET_INTERVIEW";
-
-function reducer(state, action) {
-  switch (action.type) {
-    case SET_DAY:
-      return { ...state, day: action.value }
-    case SET_APPLICATION_DATA:
-      return { ...state, ...action.value }
-    case SET_INTERVIEW: {
-      const interview = action.value.interview ? {...action.value.interview} : null;
-
-      const appointment = {
-        ...state.appointments[action.value.id],
-        interview
-      };
-      const appointments = {
-        ...state.appointments,
-        [action.value.id]: appointment
-      };
-      const days = updateDays(action.value.id, appointments, state)
-      return {...state, appointments, days}
-    }
-    default:
-      throw new Error(
-        `Tried to reduce with unsupported action type: ${action.type}`
-      );
-  }
-}
+import reducer, {
+  SET_DAY,
+  SET_APPLICATION_DATA,
+  SET_INTERVIEW
+} from "../reducers/application";
 
 export default function useApplicationData() {
   
